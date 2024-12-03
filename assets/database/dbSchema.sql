84c7b59e1b091dccb1dc44885b005f243cf9b1d5
ebbmango <ebbmango@tuta.io>
2024-12-03 10:12:40 +0100

    created the database schema

---
+++ assets/database/dbSchema.sql
@@ -0,0 +1,87 @@
+-- MEALS
+CREATE TABLE IF NOT EXISTS "meals" (
+    "id" INTEGER UNIQUE PRIMARY KEY NOT NULL,
+    "name" TEXT,
+    "isDeleted" INTEGER
+);
+-- FOODS
+CREATE TABLE IF NOT EXISTS "foods" (
+    "id" INTEGER UNIQUE PRIMARY KEY NOT NULL,
+    "name" TEXT,
+    "isDeleted" INTEGER
+);
+-- MEASUREMENT UNITS
+CREATE TABLE IF NOT EXISTS "units" (
+    "id" INTEGER UNIQUE PRIMARY KEY NOT NULL,
+    "symbol" TEXT
+);
+-- NUTRITIONAL TABLES
+CREATE TABLE IF NOT EXISTS "nutrients" (
+    "id" INTEGER UNIQUE PRIMARY KEY NOT NULL,
+    "foodId" INTEGER,
+    "unitId" INTEGER,
+    "baseMeasure" REAL,
+    "kcals" REAL,
+    "carbs" REAL,
+    "fats" REAL,
+    "protein" REAL,
+    "isDeleted" INTEGER,
+    -- A (food's) nutritional table belongs to a food. One food can have many nutritional tables.
+    FOREIGN KEY ("foodId") REFERENCES "foods" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    -- A (food's) nutritional table "belongs" to a unit. One unit can have many nutritional tables.
+    FOREIGN KEY ("unitId") REFERENCES "units" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+-- ENTRIES
+CREATE TABLE IF NOT EXISTS "entries" (
+    "id" INTEGER UNIQUE PRIMARY KEY NOT NULL,
+    "foodId" INTEGER,
+    "nutrientsId" INTEGER,
+    "date" TEXT,
+    "amount" REAL,
+    "unitId" INTEGER,
+    "mealId" INTEGER,
+    -- An entry belongs to a meal. One meal can have many entries.
+    FOREIGN KEY ("mealId") REFERENCES "meals" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    -- An entry belongs to a food. One food can have many entries.
+    FOREIGN KEY ("foodId") REFERENCES "foods" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    -- An entry belongs to a nutritional table. One nutritional table can have many entries.
+    FOREIGN KEY ("nutrientsId") REFERENCES "nutrients" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+-- Inserts measurement units.
+INSERT INTO units (symbol)
+VALUES ('g'),
+    ('ml'),
+    ('lb'),
+    ('tsp'),
+    ('tbsp'),
+    ('cup'),
+    ('oz');
+-- Inserts initial ("out-of-the-box") meals.
+INSERT INTO meals (name, isDeleted)
+VALUES ('breakfast', 0),
+    ('morning snack', 0),
+    ('lunch', 0),
+    ('afternoon snack', 0),
+    ('dinner', 0);
+-- Inserts initial foods (for ease of development only - DELETE FOR PRODUCTION)
+INSERT INTO foods (id, name, isDeleted)
+VALUES (1, 'Milk', 0),
+    (2, 'Egg', 0),
+    (3, 'Banana', 0);
+-- Inserts initial nutritional tables (for ease of development only - DELETE FOR PRODUCTION)
+INSERT INTO nutrients (
+        foodId,
+        unitId,
+        baseMeasure,
+        kcals,
+        carbs,
+        fats,
+        protein,
+        isDeleted
+    )
+VALUES -- Milk
+    (1, 2, 100, 60, 4.7, 3.2, 4.2, 0),
+    -- Egg
+    (2, 1, 100, 147, 0.77, 9.94, 12.58, 0),
+    -- Banana
+    (3, 1, 100, 89, 22.84, 0.33, 1.09, 0);
