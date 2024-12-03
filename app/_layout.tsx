6aab5a33ebd95010a04b1becc8043780c49d0557
ebbmango <ebbmango@tuta.io>
2024-12-03 10:15:31 +0100

    app has access to the database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { SQLiteProvider } from "expo-sqlite";
+import { Suspense } from "react";
+import { Text } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  <Suspense fallback={<Text>Loading...</Text>}>
+    <SQLiteProvider
+      useSuspense
+      databaseName="appDatabase.db"
+      assetSource={{
+        assetId: require("@/assets/database/appDatabase.db"),
+      }}
+    >
+      return <Stack />;
+    </SQLiteProvider>
+  </Suspense>;
 }
