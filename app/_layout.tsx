ed49efc50ab0198a88401e83834e742589c8876e
ebbmango <ebbmango@tuta.io>
2024-12-03 10:26:11 +0100

    database is managed by a query client

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,23 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { SQLiteProvider } from "expo-sqlite";
 import { Suspense } from "react";
 import { Text } from "react-native";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   <Suspense fallback={<Text>Loading...</Text>}>
-    <SQLiteProvider
-      useSuspense
-      databaseName="appDatabase.db"
-      assetSource={{
-        assetId: require("@/assets/database/appDatabase.db"),
-      }}
-    >
-      return <Stack />;
-    </SQLiteProvider>
+    <QueryClientProvider client={queryClient}>
+      <SQLiteProvider
+        useSuspense
+        databaseName="appDatabase.db"
+        assetSource={{
+          assetId: require("@/assets/database/appDatabase.db"),
+        }}
+      >
+        return <Stack />;
+      </SQLiteProvider>
+    </QueryClientProvider>
   </Suspense>;
 }
