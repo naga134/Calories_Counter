4d4c8ad60beedae75d59d33e22375b39dca44aa4
ebbmango <ebbmango@tuta.io>
2024-12-04 20:39:24 +0100

    added meal drawers list to homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,15 @@
+import IconSVG from "@/components/icons/IconSVG";
+import MealDrawer from "@/components/MealDrawer";
 import getAllMeals from "@/database/queries/mealsQueries";
 import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import { SQLiteDatabase, useSQLiteContext } from "expo-sqlite";
-import { Text, View } from "react-native";
+import { Text, View } from "react-native-ui-lib";
 
 export default function Index() {
   const database: SQLiteDatabase = useSQLiteContext();
   const queryClient: QueryClient = useQueryClient();
 
+  // Retrieving the list of daily meals from the database
   const { data: meals = [] } = useQuery({
     queryKey: ["meals"],
     queryFn: () => getAllMeals(database),
@@ -21,7 +24,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={{ gap: "16" }}>
+        {meals.map((meal) => (
+          <MealDrawer key={meal.id} mealName={meal.name} />
+        ))}
+      </View>
     </View>
   );
 }
