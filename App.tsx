61784382df1e02368229a32b91968fc22f0e67f1
ebbmango <ebbmango@tuta.io>
2024-12-07 20:43:06 +0100

    isolated navigation logic

--- App.tsx
+++ App.tsx
@@ -2,6 +2,35 @@ import 'react-native-gesture-handler';
 
 import RootStack from './navigation';
 
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DateProvider } from 'context/DateContext';
+import { ColorsProvider } from 'context/ColorContext';
+import { Suspense } from 'react';
+import { Text } from 'react-native-ui-lib';
+import { SQLiteProvider } from 'expo-sqlite';
+
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <RootStack />;
+  return (
+    <GestureHandlerRootView>
+      <QueryClientProvider client={queryClient}>
+        <SQLiteProvider
+          useSuspense
+          databaseName="appDatabase.db"
+          assetSource={{
+            assetId: require('assets/database/appDatabase.db'),
+          }}>
+          <DateProvider>
+            <ColorsProvider>
+              <Suspense fallback={<Text>Loading...</Text>}>
+                <RootStack />
+              </Suspense>
+            </ColorsProvider>
+          </DateProvider>
+        </SQLiteProvider>
+      </QueryClientProvider>
+    </GestureHandlerRootView>
+  );
 }
--- navigation/index.tsx
+++ navigation/index.tsx
@@ -3,13 +3,6 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Home from 'screens/Home';
 import FoodsList from 'screens/FoodsList';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { DateProvider } from 'context/DateContext';
-import { ColorsProvider } from 'context/ColorContext';
-import { Suspense } from 'react';
-import { Text } from 'react-native-ui-lib';
-import { SQLiteProvider } from 'expo-sqlite';
 
 export type RootStackParamList = {
   Home: undefined;
@@ -18,36 +11,17 @@ export type RootStackParamList = {
 
 const Stack = createStackNavigator<RootStackParamList>();
 
-const queryClient = new QueryClient();
-
 export default function RootStack() {
   return (
-    <GestureHandlerRootView>
-      <NavigationContainer>
-        <QueryClientProvider client={queryClient}>
-          <SQLiteProvider
-            useSuspense
-            databaseName="appDatabase.db"
-            assetSource={{
-              assetId: require('assets/database/appDatabase.db'),
-            }}>
-            <DateProvider>
-              <ColorsProvider>
-                <Suspense fallback={<Text>Loading...</Text>}>
-                  <Stack.Navigator initialRouteName="Home">
-                    <Stack.Screen name="Home" component={Home} />
-                    <Stack.Screen
-                      name="FoodList"
-                      component={FoodsList}
-                      options={{ animation: 'slide_from_right' }}
-                    />
-                  </Stack.Navigator>
-                </Suspense>
-              </ColorsProvider>
-            </DateProvider>
-          </SQLiteProvider>
-        </QueryClientProvider>
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen
+          name="FoodList"
+          component={FoodsList}
+          options={{ animation: 'slide_from_right' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
