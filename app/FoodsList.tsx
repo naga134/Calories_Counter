a04825e80d97a8c30381d66e19e292e9cbe718a8
ebbmango <ebbmango@tuta.io>
2024-12-07 14:56:13 +0100

    changed food list item design

--- app/FoodsList.tsx
+++ app/FoodsList.tsx
@@ -1,34 +1,59 @@
+import IconSVG from "@/components/icons/IconSVG";
+import MealDrawer from "@/components/MealDrawer";
+import RotatingCaret from "@/components/RotatingCaret";
+import getAllFoods from "@/database/queries/foodsQueries";
+import { Food } from "@/database/types";
+import { QueryClient, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useNavigation } from "expo-router";
 import { navigate } from "expo-router/build/global-state/routing";
+import { SQLiteDatabase, useSQLiteContext } from "expo-sqlite";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 import { Pressable, ScrollView } from "react-native-gesture-handler";
 import { Colors, Text, View } from "react-native-ui-lib";
 
 export default function FoodsList() {
-  const navigation = useNavigation();
+  const database: SQLiteDatabase = useSQLiteContext();
+  const queryClient: QueryClient = useQueryClient();
+
+  // Retrieving the list of daily meals from the database
+  const { data: foods = [] } = useQuery({
+    queryKey: ["foods"],
+    queryFn: () => getAllFoods(database),
+    initialData: [],
+  });
+
+  console.log(foods);
 
   return (
-    <ScrollView contentContainerStyle={{ padding: 20 }}>
-      <View
-        style={{
-          flexDirection: "row",
-          height: 64,
-          borderRadius: 100,
-          backgroundColor: Colors.white,
-          // justifyContent: "center",
-          alignItems: "center",
-          paddingStart: 10,
-        }}
-      >
-        <View
-          style={{
-            height: 48,
-            width: 48,
-            borderRadius: 100,
-            backgroundColor: Colors.purple50,
-          }}
-        ></View>
-        {/* <Text white>adasd</Text> */}
-      </View>
+    <ScrollView contentContainerStyle={{ padding: 20, gap: 12 }}>
+      {foods.map((food) => (
+        <FoodListItem key={food.id} food={food} />
+      ))}
     </ScrollView>
+  );
+}
+
+function FoodListItem({ food }: { food: Food }) {
+  const [rotated, setRotated] = useState(false);
+
+  return (
+    <View
+      key={food.id}
+      style={{
+        height: "auto",
+        backgroundColor: Colors.white,
+        padding: 15,
+        borderRadius: 10,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <Text style={{fontSize: 18}}>{food.name}</Text>
+      <TouchableOpacity onPress={() => setRotated(!rotated)}>
+        <RotatingCaret rotated={rotated} color={Colors.grey20} />
+      </TouchableOpacity>
+    </View>
   );
 }
